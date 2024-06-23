6da60fa8586c845bc554a86453b725780c4b4566
anawatj <ajarusiripot@gmail.com>
2024-06-24 05:12:16 +0700

    repo unit test coverage 100

--- data/customers/store_test.go
+++ data/customers/store_test.go
@@ -2,6 +2,7 @@ package customers_test
 
 import (
 	"database/sql"
+	"errors"
 	"itmx-test/data/customers"
 	domains "itmx-test/domain/customers"
 	"testing"
@@ -254,50 +255,101 @@ func TestDeleteCustomerSuccess(t *testing.T) {
 }
 
 func TestCheckDuplicateCustomerNameFound(t *testing.T) {
-	s := &v1Suite{}
-	var (
-		db  *sql.DB
-		err error
-	)
+	t.Run("it success", func(t *testing.T) {
+		s := &v1Suite{}
+		var (
+			db  *sql.DB
+			err error
+		)
 
-	db, s.mock, err = sqlmock.New()
+		db, s.mock, err = sqlmock.New()
 
-	if err != nil {
-		t.Errorf("Failed to open mock sql db, got error: %v", err)
-	}
+		if err != nil {
+			t.Errorf("Failed to open mock sql db, got error: %v", err)
+		}
 
-	if db == nil {
-		t.Error("mock db is null")
-	}
+		if db == nil {
+			t.Error("mock db is null")
+		}
 
-	if s.mock == nil {
-		t.Error("sqlmock is null")
-	}
+		if s.mock == nil {
+			t.Error("sqlmock is null")
+		}
 
-	s.db, err = gorm.Open("sqlite3", db)
-	if err != nil {
-		t.Errorf("Failed to open gorm db, got error: %v", err)
-	}
+		s.db, err = gorm.Open("sqlite3", db)
+		if err != nil {
+			t.Errorf("Failed to open gorm db, got error: %v", err)
+		}
 
-	if s.db == nil {
-		t.Error("gorm db is null")
-	}
-	customer := domains.Customer{
-		Id:   0,
-		Name: "Test",
-		Age:  10,
-	}
-	store := &customers.Store{
-		DB: s.db,
-	}
+		if s.db == nil {
+			t.Error("gorm db is null")
+		}
+		customer := domains.Customer{
+			Id:   0,
+			Name: "Test",
+			Age:  10,
+		}
+		store := &customers.Store{
+			DB: s.db,
+		}
 
-	s.mock.ExpectBegin()
-	store.CreateCustomer(&customer)
-	rows := sqlmock.NewRows([]string{"Id", "Name", "Age"}).AddRow(customer.Id, customer.Name, customer.Age)
-	s.mock.ExpectQuery(`SELECT`).WillReturnRows(rows)
-	store.CheckDuplicateCustomerName("Test", 0)
-	s.mock.ExpectCommit()
-	defer db.Close()
+		s.mock.ExpectBegin()
+		store.CreateCustomer(&customer)
+		rows := sqlmock.NewRows([]string{"Id", "Name", "Age"}).AddRow(customer.Id, customer.Name, customer.Age)
+		s.mock.ExpectQuery(`SELECT`).WillReturnRows(rows)
+		store.CheckDuplicateCustomerName("Test", 0)
+		s.mock.ExpectCommit()
+		defer db.Close()
+	})
+	t.Run("it errors", func(t *testing.T) {
+		s := &v1Suite{}
+		var (
+			db  *sql.DB
+			err error
+		)
+
+		db, s.mock, err = sqlmock.New()
+
+		if err != nil {
+			t.Errorf("Failed to open mock sql db, got error: %v", err)
+		}
+
+		if db == nil {
+			t.Error("mock db is null")
+		}
+
+		if s.mock == nil {
+			t.Error("sqlmock is null")
+		}
+
+		s.db, err = gorm.Open("sqlite3", db)
+		if err != nil {
+			t.Errorf("Failed to open gorm db, got error: %v", err)
+		}
+
+		if s.db == nil {
+			t.Error("gorm db is null")
+		}
+		customer := domains.Customer{
+			Id:   0,
+			Name: "Test",
+			Age:  10,
+		}
+		store := &customers.Store{
+			DB: s.db,
+		}
+
+		s.mock.ExpectBegin()
+		store.CreateCustomer(&customer)
+		rows := sqlmock.NewRows([]string{"Id", "Name", "Age"}).AddRow(customer.Id, customer.Name, customer.Age)
+		s.mock.ExpectQuery(`SELECT`).WillReturnRows(rows)
+		store.DB.AddError(errors.New("Database Error"))
+		store.CheckDuplicateCustomerName("Test", 0)
+
+		s.mock.ExpectCommit()
+		defer db.Close()
+	})
+
 }
 
 func TestCheckDuplicateCustomerNameNotFound(t *testing.T) {
